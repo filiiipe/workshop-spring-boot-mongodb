4980769546eacf4baffad3e577b0d4ad017c04a8
Filipe Araujo <luiz4r4ujo@gmail.com>
2024-09-03 14:34:16 -0300

    Acrescentando comentários aos posts

---
+++ src/main/java/com/luizfilipe/workshopmongo/resources/PostResource.java
@@ -0,0 +1,29 @@
+package com.luizfilipe.workshopmongo.resources;
+
+import com.luizfilipe.workshopmongo.domain.Post;
+import com.luizfilipe.workshopmongo.domain.User;
+import com.luizfilipe.workshopmongo.dto.UserDTO;
+import com.luizfilipe.workshopmongo.services.PostService;
+import com.luizfilipe.workshopmongo.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@RestController
+@RequestMapping(value = "/posts")
+public class PostResource {
+
+    @Autowired
+    private PostService service;
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<Post> findById(@PathVariable String id){
+        Post post = service.findById(id);
+        return ResponseEntity.ok().body(post);
+    }
+}
